c56adb24293570be1347d47333a7d6a81602f65d
Realxeng <realxengpandelaki@gmail.com>
2025-10-28 00:37:09 +0800

    Added remarks option, data checking

--- JSONtoPDF.js
+++ JSONtoPDF.js
@@ -5,7 +5,8 @@ const depth = require('object-depth')
 //Create the query schema
 const schema = joi.object({
     title: joi.string(),
-    cdescription: joi.string(),
+    description: joi.string(),
+    remarks: joi.string(),
     size: joi.string().valid(
         "4A0", "2A0", "A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10",
         "B0", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10",
@@ -36,19 +37,33 @@ function convert(req, res) {
     const FONT = queryValue.font
     //Get the data
     const data = req.body
+    //Check the data
+    if (!data) {
+        console.log('Request body: ')
+        console.log(req.body)
+        return res.status(400).json({ message: "No JSON to parse" })
+    }
+    //Find the max depth
+    const maxDepth = depth(data)
+    if (!maxDepth || maxDepth < 1) {
+        console.log("Data: ")
+        console.log(data)
+        return res.status(400).json({ message: "Incomplete JSON" })
+    }
+
     //Intialize the PDF
     const doc = new PDF({
         size: queryValue.size,
         margins: queryValue.margins
     })
-    //Set the response header
-    res.setHeader("Content-Type", "application/pdf")
-    res.setHeader("Content-Disposition", `inline; filename=form.pdf`)
-    res.status(200)
     //Create the doc metadata
     doc.info({
         title: queryValue.title
     })
+    //Set the response header
+    res.setHeader("Content-Type", "application/pdf")
+    res.setHeader("Content-Disposition", `inline; filename=form.pdf`)
+    res.status(200)
     //Send the pdf to res object
     doc.pipe(res)
 
@@ -58,56 +73,51 @@ function convert(req, res) {
     //Title
     if (queryValue.title) doc.font(FONT).fontSize(18).text(queryValue.title, { align: 'center' })
     //Description
-    if (queryValue.description) doc.font(FONT).fontSize(11).text(queryValue.description)
+    if (queryValue.description) doc.font(FONT).fontSize(12).text(queryValue.description)
 
-    //Function to create key value pair
-    const childKeyValue = (childKey, childValue, colSpan) => {
+    //Functions to create key value pair
+    const childKeyValue = (childKey, childValue, colSpan, first) => {
         if (Array.isArray(value)){
-            const rowSpan = value.length
-            td.push([{ rowSpan, text: key }, { colSpan, text: value[0] }])
-            value.forEach(element => {
-                td.push([element])
-            })
+            
         } else if (typeof childValue === 'object') {
-            for (const grandchildKey in childValue) {
-                childKeyValue(grandchildKey, childValue[childKey], colSpan - 1)
-            }
+            const rowSpan = Object.keys(childValue).length
+            if (first) td.at(-1).push({rowSpan, text: childKey})
+            Object.keys(childValue).forEach( (grandChildKey, index) => {
+                childKeyValue(grandChildKey, childValue[grandChildKey], colSpan - 1, index === 0)
+            })
         } else {
-            td.push([key, { colSpan, text: value}])
+            
         }
     }
-
     const createKeyValue = (key, value, colSpan) => {
         if (Array.isArray(value)){
             const rowSpan = value.length
-            td.push([{ rowSpan, text: key }, { colSpan, text: value[0] }])
-            value.forEach(element => {
-                td.push([element])
-            })
+            td.push([{ rowSpan, text: key }])
+            value.forEach( (element, index) => {
+                childKeyValue(index + 1, element, colSpan - 1, (index === 0))
+            });
         } else if (typeof value === 'object') {
-            for (const childKey in value) {
-                childKeyValue(childKey, value[childKey], colSpan - 1)
-            }
+            const rowSpan = value.length
         } else {
-            td.push([key, { colSpan, text: value}])
+            td.push([key, value])
         }
     }
 
-    //Find the max depth
-    const maxDepth = depth(data)
-    //Create the table
+    //Build the table data
     const td = []
-    //Table data
     for (const key in data) {
-        createKeyValue(key, data[key], maxDepth)
+        createKeyValue(key, data[key], maxDepth, true)
     }
+    //Create the table
     doc.table({
         data: td
     })
 
+    //Remarks
+    if (queryValue.remarks) doc.font(FONT).fontSize(12).text(queryValue.remarks)
+
     //Close the doc
     doc.end()
-
 }
 
 module.exports = convert
