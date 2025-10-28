89852a83cd73e841f0b39672fd14c7cc2867c239
Realxeng <realxengpandelaki@gmail.com>
2025-10-28 18:42:51 +0800

    Update paper size to accept array of width and height, changed default margins to margin

--- JSONtoPDF.js
+++ JSONtoPDF.js
@@ -14,25 +14,26 @@ const schema = joi.object({
     ownerPassword: joi.string(),
     pdfVersion: joi.string(),
     autoFirstPage: joi.bool(),
-    size: joi.string().valid(
-        "4A0", "2A0", "A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10",
-        "B0", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10",
-        "C0", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10",
-        "RA0", "RA1", "RA2", "RA3", "RA4",
-        "SRA0", "SRA1", "SRA2", "SRA3", "SRA4",
-        "LETTER", "LEGAL", "TABLOID", "EXECUTIVE", "FOLIO"
+    size: joi.alternatives().try(
+        joi.string().valid(
+            "4A0", "2A0", "A0", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10",
+            "B0", "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10",
+            "C0", "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10",
+            "RA0", "RA1", "RA2", "RA3", "RA4",
+            "SRA0", "SRA1", "SRA2", "SRA3", "SRA4",
+            "LETTER", "LEGAL", "TABLOID", "EXECUTIVE", "FOLIO"
+        ),
+        joi.array().items(joi.number()).min(2).max(2)
     ).default("A4"),
-    margin: joi.number().integer(),
+    margin: joi.alternatives().try(
+        joi.number(),
+        joi.string()
+    ).default(50),
     margins: joi.object({
         top: joi.number().min(0),
         bottom: joi.number().min(0),
         left: joi.number(),
         right: joi.number(),
-    }).default({
-        top: 50,
-        bottom: 50,
-        left: 50,
-        right: 50
     }),
     layout: joi.string().valid("potrait", "landscape"),
     font: joi.string().valid(
@@ -105,9 +106,9 @@ function convert(req, res) {
             obj.forEach((value, index) => {
                 if (typeof value === 'object') {
                     const rowSpan = Object.keys(flatten(value)).length
-                    !td.length ? td.push([{ rowSpan, align: { x:'center', y: 'center' }, text: index + 1 }])
-                        : index === 0 ? td.at(-1).push({ rowSpan, align: { x:'center', y: 'center' }, text: index + 1 })
-                            : td.push([{ rowSpan, align: { x:'center', y: 'center' }, text: index + 1 }]);
+                    !td.length ? td.push([{ rowSpan, align: { x: 'center', y: 'center' }, text: index + 1 }])
+                        : index === 0 ? td.at(-1).push({ rowSpan, align: { x: 'center', y: 'center' }, text: index + 1 })
+                            : td.push([{ rowSpan, align: { x: 'center', y: 'center' }, text: index + 1 }]);
                     traverseJSON(value, depth - 1);
                 }
                 //Value
