d725c3665f202920e6ed58d6fee32ca8c171bd6a
Realxeng <realxengpandelaki@gmail.com>
2025-11-04 13:19:44 +0800

    Initialize PDFGenerator

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const PORT = process.env.PORT || 3000;
-const JSONtoPDF = require('./JSONtoPDF')
+const JSONtoPDF = require('./JSONtoPDF/JSONtoPDF')
+const createPDF = require('./PDFGenerator/controller/createController')
+const template = require('./PDFGenerator/model/templateNocobase')
+const data = require('./PDFGenerator/model/dataNocobase')
 
 //Intialize Express Server
 const app = express()
@@ -8,6 +11,18 @@ app.use(express.json())
 
 //Route to convert JSON to PDF
 app.post('/jsontopdf', JSONtoPDF)
+
+/**
+ * Generate and fill pdf from template
+ */
+//UI for creating tagging the textbox
+app.get('/create', createPDF)
+//Save the textbox template
+app.post('/template', template.create)
+//Get the textbox template
+app.get('/template', template.get)
+//Get the data from Nocobase
+app.get('/data', data.get)
 
 //Unknown route
 app.use('/', (req, res) => {
