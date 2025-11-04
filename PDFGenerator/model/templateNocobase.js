1b962456bfc201069c1c4a08f15dcb9a8ae76a34
Jinxsuxk <marvinjaylorenz@gmail.com>
2025-11-04 13:35:40 +0800

    initialize create and get

--- PDFGenerator/model/templateNocobase.js
+++ PDFGenerator/model/templateNocobase.js
@@ -1,5 +1,12 @@
+const create = async(req, res) => {
+    const body = req.body
+    return res.status(200).send('bakekok')
+}
 
-
+const get = async (req, res) => {
+    const body = req.body
+    return res.status(200).send('kokekab')
+}
 
 module.exports = {
     create, get
