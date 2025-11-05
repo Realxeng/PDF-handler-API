4ebb8922c5eae29b1f8c33420c4c364b5bcbf427
Realxeng <realxengpandelaki@gmail.com>
2025-11-05 15:30:31 +0800

    Imported nocobase functions

--- PDFGenerator/controller/fillController.js
+++ PDFGenerator/controller/fillController.js
@@ -1,6 +1,8 @@
 const joi = require('joi')
 const PDFHelper = require('../logic/PDFHelper')
 
+const schema = joi.object()
+
 async function fillController(req, res) {
     
 }
---
+++ PDFGenerator/logic/NocobaseFunctions.js
@@ -0,0 +1,283 @@
+const joi = require("joi");
+const _ = require("lodash");
+const util = require("util");
+/**
+ * A class to do CRUD operation into nocobase data source
+ */
+class NocobaseFunctions {
+    /**
+     * Create a new instance of NocobaseFunctions class to use CRUD methods on a specific entity in NocoBase
+     * 
+     * @param {string} nocoTable - The corresponding table name in NocoBase.
+     * @param {string} [name=nocoTable] - A readable name for the entity (defaults to nocoTable).
+     * @param {string} [nocoUrl=process.env.DATABASE_URI || 'http://localhost:13000/'] - The base URL of the NocoBase instance.
+     */
+
+    constructor(nocoTable, name = nocoTable, nocoUrl = process.env.DATABASE_URI || 'http://localhost:13000/') {
+        this.nocoTable = nocoTable   /* Table name in Nocobase */
+        this.name = name             /* General name of the entity */
+        this.nocoUrl = nocoUrl       /* Nocobase endpoint */
+    }
+
+    /**
+     * Insert and Update data to Nocobase
+     * 
+     * @async
+     * @param {{values: object[], cred: { NOCOBASE_TOKEN: string, NOCOBASE_APP: string, DATABASE_URI: string } }} body - The request body retrieved from the API request
+     * @param {import('joi').ObjectSchema} structure - The Joi validation schema for the request body
+     * @returns {Promise<{ status: number, json: | { message: string, errors: Array<object> }
+     *                                           | { message: string }
+     *                                           | { data: string } }>}
+     */
+    async upload(body, structure) {
+        const credentials = body.cred || process.env
+        //Verify all credentials exists
+        const { error: credError, value: cred } = validateCredentials(credentials)
+        if (credError) return { status: 401, json: { message: credError.details.map(d => d.message).join(", "), errors: credError.details } }
+        //Get the credentials
+        const { NOCOBASE_TOKEN, NOCOBASE_APP, DATABASE_URI } = cred
+
+        //Validate the request
+        const { error, value: data } = structure.validate(body)
+        //console.log(error)
+        //Catch validation error
+        if (error) {
+            console.log(error)
+            return { status: 400, json: { message: error.details, error } }
+        }
+
+        //Get existing data
+        const { records: nocoData, message } = await this.getAll(cred)
+        if (message) {
+            return { status: 500, json: { message } }
+        }
+        const existingIdset = new Set(nocoData.map(d => d.id))
+
+        //Split the data into create and update
+        const createPayload = []
+        const updatePayload = []
+        //Check for existing data
+        //console.log(data)
+        for (const record of data.values) {
+            if (existingIdset.has(record.id)) {
+                updatePayload.push(record)
+            } else {
+                createPayload.push(record)
+            }
+        }
+
+        let updatedLength = 0
+        let createdLength = 0
+
+        //Update data
+        if (updatePayload.length > 0) {
+            console.log(`Updating ${updatePayload.length} ${this.name} to nocobase`)
+            //Iterate through the old data and update each field
+            const updateIDs = new Set(updatePayload.map(record => record.id))
+            const oldData = nocoData.filter(record => updateIDs.has(record.id))
+            const updatedData = oldData.map(oldRecord => {
+                const newRecord = updatePayload.find(newData => newData.id === oldRecord.id)
+                return _.merge({}, oldRecord, newRecord)
+            })
+
+            //Delete the old data
+            console.log(`Deleting record(s) ${[...updateIDs].join(', ')}`)
+            try {
+                const filter = {
+                    id: {
+                        $in: [...updateIDs]
+                    }
+                }
+                const deleteResponse = await fetch(`${this.nocoUrl}api/${this.nocoTable}:destroy?filter=${encodeURIComponent(JSON.stringify(filter))}`, {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": 'application/json',
+                        'Accept': 'application/json',
+                        Authorization: `Bearer ${NOCOBASE_TOKEN}`,
+                        'X-App': NOCOBASE_APP,
+                        'X-Host': DATABASE_URI,
+                    },
+                })
+                const deleteResponseJSON = await deleteResponse.json()
+                //console.log(util.inspect(deleteResponseJSON, { depth: null, colors: true }))
+                //Check for errors
+                if (deleteResponseJSON.errors) {
+                    return { status: 400, json: { message: deleteResponseJSON.errors.map(e => e.message).join(', ') } }
+                }
+            } catch (error) {
+                console.log(error)
+                return { status: 500, json: { message: `Failed to delete old ${this.tableName} on nocobase` } }
+            }
+
+            //Insert the new data
+            console.log(`Inserting record(s) ${[...updateIDs].join(', ')}`)
+            try {
+                const createResponse = await fetch(`${this.nocoUrl}api/${this.nocoTable}:create`, {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": 'application/json',
+                        'Accept': 'application/json',
+                        Authorization: `Bearer ${NOCOBASE_TOKEN}`,
+                        'X-App': NOCOBASE_APP,
+                        'X-Host': DATABASE_URI,
+                    },
+                    body: JSON.stringify(updatedData)
+                })
+                const createResponseJSON = await createResponse.json()
+                //console.log(util.inspect(createResponseJSON, { depth: null, colors: true }))
+                //Check for errors
+                if (createResponseJSON.errors) {
+                    return { status: 400, json: { message: createResponseJSON.errors[0].message, errors: createResponseJSON.errors } }
+                }
+
+                //Return the number of successfully replaced data
+                updatedLength = createResponseJSON.data?.length
+            } catch (error) {
+                console.log(error)
+                return { status: 500, json: { message: `Failed to insert new ${this.name} to nocobase` } }
+            }
+        }
+
+        //Create data
+        if (createPayload.length > 0) {
+            console.log(`Creating ${createPayload.length} data to nocobase`)
+            try {
+                const response = await fetch(`${this.nocoUrl}api/${this.nocoTable}:create`, {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": 'application/json',
+                        'Accept': 'application/json',
+                        Authorization: `Bearer ${NOCOBASE_TOKEN}`,
+                        'X-App': NOCOBASE_APP,
+                        'X-Host': DATABASE_URI,
+                    },
+                    body: JSON.stringify(createPayload)
+                })
+                const responseJson = await response.json()
+                //Check for errors
+                if (responseJson.errors) {
+                    return { status: 400, json: { message: responseJson.errors[0].message, errors: responseJson.errors } }
+                }
+
+                //Return the number of created data
+                const dataLength = responseJson.data ? responseJson.data.length : 0
+                createdLength = dataLength
+            } catch (error) {
+                console.log(error)
+                return { status: 500, json: { message: `Error creating ${this.name} on nocobase` } }
+            }
+        }
+        return { status: 201, json: { data: `Data updated: ${updatedLength} entries, Data created: ${createdLength} entries` } }
+    }
+
+    /**
+     * Deletes data from Nocobase
+     * 
+     * @async
+     * @param {{id: number[], cred: { NOCOBASE_TOKEN: string, NOCOBASE_APP: string, DATABASE_URI: string }}} body - The request body retrieved from the API request
+     * @returns {Promise<{ status: number, json: | { message: string, errors: Array<object> }
+     *                                           | { message: string }
+     *                                           | { data: string } }>}
+     */
+    async delete(body) {
+        const credentials = body.cred || process.env
+        //Validate the request
+        const deleteStructure = joi.object({
+            id: joi.array().items(joi.number().integer()).min(1),
+            cred: joi.strip(),
+        })
+        const { error, value } = deleteStructure.validate(body, { abortEarly: false })
+        if (error) return { status: 400, json: { message: error.details.map(d => d.message).join(", "), errors: error.details } }
+
+        //Verify the credentials
+        const { error: credError, value: cred } = validateCredentials(credentials)
+        if (credError) {
+            return { status: 401, json: { message: credError.details.map(d => d.message).join(", "), errors: credError.details } }
+        }
+
+        //Get the credentials
+        const { NOCOBASE_TOKEN, NOCOBASE_APP, DATABASE_URI } = cred
+
+        //Create the payload for nocobase
+        const filter = {
+            id: {
+                $in: value.id.map(id => String(id))
+            }
+        }
+
+        //Delete from Nocobase
+        const responseNB = await fetch(`${this.nocoUrl}api/${this.nocoTable}:destroy?filter=${encodeURIComponent(JSON.stringify(filter))}`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json',
+                'Accept': 'application/json',
+                Authorization: `Bearer ${NOCOBASE_TOKEN}`,
+                'X-App': NOCOBASE_APP,
+                'X-Host': DATABASE_URI,
+            },
+        })
+
+        const responseNBJson = await responseNB.json()
+        //console.log(util.inspect(responseNBJson, { depth: null, colors: true }));
+        //Check for errors from Nocobase
+        if (responseNBJson.errors) {
+            return { status: 400, json: { message: responseNBJson.errors.map(e => e.message).join(', ') } }
+        }
+        console.log(`Deleted successfully`)
+        //Return the number of deleted data
+        const dataLength = responseNBJson.data
+        return { status: 200, json: { data: `Deleted ${dataLength} record(s)` } }
+    }
+
+    /**
+     * Gets all data from the entity in NocoBase
+     * 
+     * @param {{NOCOBASE_TOKEN: string, NOCOBASE_APP: string, DATABASE_URI: string}} cred - The authentication and connection credentials to connect to NocoBase
+     * @returns {Promise<{ records?: object[], message?: string }>}
+     */
+    async getAll(cred) {
+        const { error, value } = validateCredentials(cred)
+        if (error) {
+            console.log(error)
+            return res.status(400).json({ message: "Invalid nocobase credentials", error })
+        }
+        const { NOCOBASE_TOKEN, NOCOBASE_APP, DATABASE_URI } = value
+        let records = [], data = []
+        let page = 1
+        do {
+            const res = await fetch(`${this.nocoUrl}api/${this.nocoTable}:list?page=${page}`, {
+                method: 'GET',
+                headers: {
+                    "Content-Type": 'application/json',
+                    'Accept': 'application/json',
+                    Authorization: `Bearer ${NOCOBASE_TOKEN}`,
+                    'X-App': NOCOBASE_APP,
+                    'X-Host': DATABASE_URI,
+                },
+            })
+            data = await res.json()
+            records = records.concat(data.data)
+            page++
+        }
+        while (data.meta && data.meta.totalPage >= page);
+
+        if (data.error) {
+            return { message: data.error }
+        }
+
+        return { records }
+    }
+}
+
+//Validate Nocobase credentials
+function validateCredentials(credentials) {
+    const structure = joi.object({
+        NOCOBASE_TOKEN: joi.string().required(),
+        NOCOBASE_APP: joi.string().required(),
+        DATABASE_URI: joi.string().required(),
+    }).options({ stripUnknown: true })
+
+    return structure.validate(credentials, { abortEarly: false })
+}
+
+module.exports = NocobaseFunctions
