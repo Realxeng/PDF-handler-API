8aa16c7cc067bac5946e2fb3454ae907b0ad6fa8
Realxeng <realxengpandelaki@gmail.com>
2025-11-05 14:02:09 +0800

    Added addTextBox function

--- PDFGenerator/logic/createPDF.js
+++ PDFGenerator/logic/createPDF.js
@@ -1,16 +1,28 @@
-const { PDFDocument } = require('pdf-lib')
+const { PDFDocument, PDFForm } = require('pdf-lib')
 
 class CreatePDF {
-    constructor(pdfDoc) {
+    /**
+     * A helper class to build pdf forms
+     * @param {PDFDocument} pdfDoc 
+     * @param {PDFForm} form 
+     */
+    constructor(pdfDoc, form) {
         this.pdf = pdfDoc
+        this.form = form
     }
 
     static async load(pdfBuffer) {
         const pdfDoc = await PDFDocument.load(pdfBuffer)
-        return new CreatePDF(pdfDoc)
+        const form = pdfDoc.getForm()
+        return new CreatePDF(pdfDoc, form)
     }
 
-    
+    addTextBox(name, pageNum, x, y, width, height) {
+        const pages = this.pdf.getPages()
+        const page = pages[pageNum]
+        const textbox = this.form.createTextField(name)
+        textbox.addToPage(page, { x, y, width, height, borderWidth: 0 })
+    }
 }
 
 module.exports = CreatePDF
