57ebfb6870b708f041c157b4cd99a4140601c6b5
Realxeng <realxengpandelaki@gmail.com>
2025-11-05 15:03:42 +0800

    seperated joi schema with the validation

--- PDFGenerator/controller/createController.js
+++ PDFGenerator/controller/createController.js
@@ -1,41 +1,50 @@
 const joi = require('joi')
-const CreatePDF = require('../logic/createPDF')
+const PDFHelper = require('../logic/PDFHelper')
 const template = require('../model/template')
 
-async function createController (req, res) {
+const schema = joi.array().items(joi.object({
+    formField: joi.object({
+        name: joi.string(),
+        pageNum: joi.number().integer(),
+        x: joi.number(),
+        y: joi.number(),
+        width: joi.number(),
+        height: joi.number()
+    }),
+    dataField: joi.string()
+}).unknown(false)).min(1)
+
+async function createController(req, res) {
     //Get the pdf file
     const pdfBuffer = req.file.buffer
     //Get the form fields
     let fields = req.body.fields || false
 
-    //Check for form fields validation
+    //Check for empty form fields
     if (!fields) {
         console.log(fields)
         return res.status(400).json({ message: "No form fields found sent" })
     }
-    const { error, value } = joi.array().items(joi.object({
-        field: joi.object({
-            name: joi.string(),
-            pageNum: joi.number().integer(),
-            x: joi.number(),
-            y: joi.number(),
-            width: joi.number(),
-            height: joi.number()
-        }),
-        data: joi.string()
-    }).unknown(false)).min(1).validate(req.body.fields || false, { abortEarly: false })
+
+    //Validate form fields
+    const { error, value } = schema.validate(fields, { abortEarly: false })
+    //Check validation error
     if (error) {
         console.log(error)
         return res.status(400).json({ message: "Invalid request", error })
     }
+
+    //Get validated form fields
     fields = value
 
     //Load the pdf
-    const PDFForm = await CreatePDF.load(pdfBuffer)
+    const PDFForm = await PDFHelper.load(pdfBuffer)
+
     //Build the form inside pdf
-    fields.forEach((element, index) => {
-        PDFForm.addTextBox(...element.field)
+    fields.forEach((field, index) => {
+        PDFForm.addTextBox(...field.formField)
     });
+    
     //Export the pdf template with form
     const pdfFormBuffer = await PDFForm.export()
     //Create the fields template
@@ -43,7 +52,7 @@ async function createController (req, res) {
     //Upload the template
     const response = await template.upload(pdfFormBuffer)
     //Check response
-    if (response.status != 201){
+    if (response.status != 201) {
         return res.status(400).json({ message: "Failed to save template" })
     }
     //Return the template
