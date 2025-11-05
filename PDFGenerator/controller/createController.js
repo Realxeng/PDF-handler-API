d41637e35940d2553d8b4a756bde13fbf5bf8410
Realxeng <realxengpandelaki@gmail.com>
2025-11-05 12:49:52 +0800

    Add CreatePDF class to modify the pdf

--- PDFGenerator/controller/createController.js
+++ PDFGenerator/controller/createController.js
@@ -1,8 +1,8 @@
 const CreatePDF = require('../logic/createPDF')
 
 async function createController (req, res) {
-    const rawpdf = await req.body.arrayBuffer()
-    const PDFForm = new CreatePDF(req.body)
+    const pdfBuffer = req.file.buffer
+    const PDFForm = await CreatePDF.load(pdfBuffer)
 }
 
 module.exports = createController
--- PDFGenerator/logic/createPDF.js
+++ PDFGenerator/logic/createPDF.js
@@ -1,7 +1,16 @@
-class CreatePDF {
-    constructor(pdf) {
+const { PDFDocument } = require('pdf-lib')
 
+class CreatePDF {
+    constructor(pdfDoc) {
+        this.pdf = pdfDoc
     }
+
+    static async load(pdfBuffer) {
+        const pdfDoc = await PDFDocument.load(pdfBuffer)
+        return new CreatePDF(pdfDoc)
+    }
+
+    
 }
 
 module.exports = CreatePDF
