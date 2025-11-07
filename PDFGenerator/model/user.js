5acb81c88532132c00d31f32572f88bb0424adaa
Realxeng <realxengpandelaki@gmail.com>
2025-11-07 16:06:44 +0800

    fixed bugs

--- PDFGenerator/model/user.js
+++ PDFGenerator/model/user.js
@@ -1,8 +1,8 @@
 const NocobaseFunctions = require('../logic/NocobaseFunctions')
-const user = new NocobaseFunctions(users, Users, process.env.USERNOCOURL)
+const user = new NocobaseFunctions('users', 'Users', process.env.USERNOCOURL)
 
-async function get(cred, uid) {
-    const cred = cred || { NOCOBASE_TOKEN: process.env.USERNOCOTOKEN, NOCOBASE_APP: process.env.USERNOCOAPP, DATABASE_URI: process.env.USERNOCOHOST }
+async function get(credentials, uid) {
+    const cred = credentials || { NOCOBASE_TOKEN: process.env.USERNOCOTOKEN, NOCOBASE_APP: process.env.USERNOCOAPP, DATABASE_URI: process.env.USERNOCOHOST }
     const response = await user.get(cred, uid)
     return response
 }
