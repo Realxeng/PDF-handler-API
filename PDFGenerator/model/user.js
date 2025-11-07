a7d30ca695707c5f8f07be758c0e7f1b8cbee25c
stanleywijayaa <stanleytan2018@gmail.com>
2025-11-07 15:50:34 +0800

    move login function to user js

--- PDFGenerator/model/user.js
+++ PDFGenerator/model/user.js
@@ -7,6 +7,38 @@ async function get(cred, uid) {
     return response
 }
 
+async function verifyUser(acc, pwd){
+    const loginUrl = `${process.env.USERNOCOURL}api/auth:signIn`
+
+    //Post the credentials to nocobase
+    const response = await fetch(loginUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Authenticator': 'basic',
+            'X-App': process.env.USERNOCOAPP
+        },
+        body: JSON.stringify({
+            account: acc,
+            password: pwd,
+        })
+    })
+        //Check for rejection
+    if (!response.ok) {
+        const errorText = await response.text();
+        console.error("NocoBase login failed:", errorText);
+        return res.status(response.status).json({
+            message: "Invalid credentials or failed to connect to NocoBase.",
+        });
+    }
+    const data = await response.json();
+    return {
+    id: data?.data?.user?.id,
+    nickname: data?.data?.user?.nickname,
+  };
+}
+
 module.exports = {
-    get
+    get,
+    verifyUser
 }
